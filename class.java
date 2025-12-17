7dc881437fd331f6e158192a1eb28a789358c64d
varunsai1443 <75156026+varunsai1443@users.noreply.github.com>
2025-12-17 22:55:37 +0530

    Create class.java

---
+++ class.java
@@ -0,0 +1,16 @@
+public class Student {
+	public static void main(String[] args) {
+		int rollno = 1596;
+		String sname = "Varun";
+		String course = "PYTHON";
+		System.out.println("LEARN HERE AND LEAD ANYWHERE!!!");
+		System.out.println("My Name is: " + sname);
+		System.out.println("My Course is: " + course);
+		System.out.println("My Roll Number is: " + rollno);
+		System.out.println("============================");
+		System.out.println("WELCOME TO JENKINS");
+		System.out.println("GOOD EVENING");
+		
+		
+		}
+	}
